29d9b2cccba8d4a7c5bffd2cff27f815f8739c9e
David Escarcega <davidaescarcega@gmail.com>
2025-10-13 08:52:16 -0700

    ReactFront
    Agregado el proyecto de React al frontend

---
+++ frontend/tecmatch-frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,49 @@
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import styles from "./LoginPage.module.css";
+
+const LoginPage = () => {
+  return (
+    <Container className={styles.loginContainer} fluid>
+      <Row className="justify-content-center">
+        <Col className="text-center">
+          <h1 className={`${styles.title} text-shadow`}>App de chat</h1>
+          <h2 className={`${styles.subtitle} text-shadow`}>Iniciar sesión</h2>
+
+          <Form>
+            <Form.Group controlId="formUsername">
+              <Form.Label className={styles.formLabel}>Usuario</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Correo electrónico"
+                className={styles.formInput}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formPassword">
+              <Form.Label className={styles.formLabel}>Contraseña</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="ingresa tu contraseña"
+                className={styles.formInput}
+              />
+            </Form.Group>
+
+            <Button variant="light" className={styles.customButton}>
+              Ingresar
+            </Button>
+          </Form>
+
+          <div className={styles.separator}>
+            <div className={styles.separatorCircle}></div>
+          </div>
+
+          <Button variant="light" className={styles.customButton}>
+            ¡Regístrate!
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default LoginPage;
