4e39aca2d1c3011f36b0cbca40f9720e6c215244
BananaFarman <david_ec22@hotmail.com>
2025-10-14 16:10:01 -0700

    pagina centrada

--- frontend/tecmatch-frontend/src/main.tsx
+++ frontend/tecmatch-frontend/src/main.tsx
@@ -3,9 +3,8 @@ import { createRoot } from "react-dom/client";
 
 // 1. Frameworks
 import "bootstrap/dist/css/bootstrap.min.css";
-// 2. Tus estilos globales
+// 2. estilos globales
 import "./styles/global.css";
-import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
