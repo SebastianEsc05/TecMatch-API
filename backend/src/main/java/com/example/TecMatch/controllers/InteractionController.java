fd023311a6a0a2c7cf364911be2571f0a1b2f52e
SebastianEsc05 <sebastianescram01@gmail.com>
2025-10-15 14:57:09 -0700

    Implementación de editar correo de usuario registrado

--- backend/src/main/java/com/example/TecMatch/controllers/InteractionController.java
+++ backend/src/main/java/com/example/TecMatch/controllers/InteractionController.java
@@ -1,9 +1,7 @@
 package com.example.TecMatch.controllers;
 
-import com.example.TecMatch.DTOs.SolicitarDislike;
-import com.example.TecMatch.DTOs.SolicitarLike;
+import com.example.TecMatch.DTOs.SolicitarLikeDTO;
 import com.example.TecMatch.models.Dislike;
-import com.example.TecMatch.repositories.DislikeRepository;
 import com.example.TecMatch.repositories.LikeRepository;
 import com.example.TecMatch.repositories.MatchRepository;
 import com.example.TecMatch.repositories.UsuarioRepository;
@@ -28,19 +26,16 @@ public class InteractionController {
     private LikeRepository likeRepository;
 
     @Autowired
-    private DislikeRepository dislikeRepository;
-
-    @Autowired
     private MatchRepository matchRepository;
 
     @Autowired
     private UsuarioRepository usuarioRepository;
 
     @PostMapping("/likes")
-    public ResponseEntity<?> darLike(@AuthenticationPrincipal Usuario usuarioQueDaLike, @RequestBody SolicitarLike solicitarLike) {
+    public ResponseEntity<?> darLike(@AuthenticationPrincipal Usuario usuarioQueDaLike, @RequestBody SolicitarLikeDTO solicitarLikeDTO) {
 
         Long usuarioQueDaLikeId = usuarioQueDaLike.getId();
-        Long usuarioAueRecibeLikeId = solicitarLike.getUsuarioQueRecibeLikeId();
+        Long usuarioAueRecibeLikeId = solicitarLikeDTO.getUsuarioQueRecibeLikeId();
 
         if (usuarioQueDaLikeId.equals(usuarioAueRecibeLikeId)) {
             return ResponseEntity.badRequest().body("No te puedes dar auto like");
@@ -71,30 +66,4 @@ public class InteractionController {
         return ResponseEntity.ok(respuesta);
     }
 
-    @PostMapping("/dislikes")
-    public ResponseEntity<?> darDislike(@AuthenticationPrincipal Usuario usuarioQueDaDislike, @RequestBody SolicitarDislike solicitarDislike){
-
-        Long usuarioQueDaDislikeId = usuarioQueDaDislike.getId();
-        Long usuarioQueRecibeDislikeId = solicitarDislike.getUsuarioQueRecibeDislikeId();
-
-        if (usuarioQueDaDislikeId.equals(usuarioQueRecibeDislikeId)) {
-            return ResponseEntity.badRequest().body("No te puedes dar auto like");
-        }
-
-        Usuario usuarioReceptorDeLike = usuarioRepository.findById(usuarioQueRecibeDislikeId).orElseThrow(() -> new RuntimeException(("usuario que da like no encontrado")));
-
-        Dislike nuevoDislike = new Dislike();
-        nuevoDislike.setDisliker(usuarioQueDaDislike);
-        nuevoDislike.setDisliked(usuarioReceptorDeLike);
-        nuevoDislike.setFecha_hora(LocalDateTime.now());
-        dislikeRepository.save(nuevoDislike);
-
-        matchRepository.findMatchByUsuarios(usuarioQueDaDislikeId, usuarioQueRecibeDislikeId).ifPresent(match -> matchRepository.delete(match));
-
-        likeRepository.deleteByLikerIdAndLikedId(usuarioQueDaDislikeId, usuarioQueRecibeDislikeId);
-
-        Map<String, Object> respuesta = new HashMap<>();
-        respuesta.put("mensaje", "Dislike registrado de forma correcta, si habia match anteriormente, se ha elliminado");
-        return ResponseEntity.ok(respuesta);
-    }
 }
