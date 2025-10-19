8df476c7b9b9f7d2aab0fee35b43e8f10747bd65
mcortezv <mdjesuscv@gmail.com>
2025-10-18 18:08:40 -0700

    Navegacion Completa

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,10 +1,38 @@
-
+import { Link } from "react-router-dom";
+import Section from "../componets/Section";
+import H1 from "../componets/H1";
+import H2 from "../componets/H2";
+import Label from "../componets/Label";
+import Input from "../componets/Input";
+import Button from "../componets/Button";
 
 const RegisterPage = () => {
-
   return (
-    
-    null
+    <Section>
+      <H1>Registro</H1>
+      <H2>Tec-Match</H2>
+
+      <form>
+        <div className="mb-4 text-center">
+          <Label htmlFor="username">Usuario</Label>
+          <Input id="username" type="text" placeholder="Correo electrónico" />
+        </div>
+
+        <div className="mb-4 text-center">
+          <Label htmlFor="password">Contraseña</Label>
+          <Input
+            id="password"
+            type="password"
+            placeholder="Ingresa tu contraseña"
+          />
+        </div>
+
+        <Link to={"/explorePage"}>
+          <Button>Registrar</Button>
+        </Link>
+
+      </form>
+    </Section>
   );
 };
 
--- frontend/src/pages/WelcomePage.tsx
+++ frontend/src/pages/WelcomePage.tsx
@@ -1,16 +1,20 @@
 import { Link } from "react-router-dom";
+import Section from "../componets/Section";
+import H1 from "../componets/H1";
+import H2 from "../componets/H2";
+import Button from "../componets/Button";
 
 const WelcomePage = () => {
+  
   return (
-    <div className="flex justify-center items-start min-h-screen text-white text-center p-4">
-      <div className="w-full max-w-md lg:max-w-2xl">
-        <Link to={"/loginPage"}>
-          <button className="font-app-font bg-white text-black border-4 border-black rounded-full py-2 px-8 text-2xl mt-8 mb-8 shadow-[-6px_6px_0px_black] transition-transform duration-100 ease-in-out hover:translate-x-0.5 hover:translate-y-0.5 hover:shadow-[-2px_2px_0px_black]">
-            Comenzar
-          </button>
-        </Link>
-      </div>
-    </div>
+
+    <Section>
+      <H1>Bienvenido!</H1>
+      <H2>Tec-Match</H2>
+      <Link to={"/loginPage"}>
+        <Button>Comenzar</Button>
+      </Link>
+    </Section>
   );
 };
 
