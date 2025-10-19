5200e96adfcd958a218be2f6824015298df2e1d8
mcortezv <mdjesuscv@gmail.com>
2025-10-18 17:18:39 -0700

    Componetizacion y Navegacion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import WelcomePage from "./pages/WelcomePage";
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ExplorePage from "./pages/ExplorePage";
+import MatchesPage from "./pages/MatchesPage";
+import ChatPage from "./pages/ChatPage";
 
 function App() {
-  return <LoginPage />;
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/loginPage" element={<LoginPage />} />
+          <Route path="/registerPage" element={<RegisterPage />} />
+          <Route path="/explorePage" element={<ExplorePage />} />
+          <Route path="/matchesPage" element={<MatchesPage />} />
+          <Route path="/chatPage" element={<ChatPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
