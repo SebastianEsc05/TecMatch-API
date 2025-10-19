ce183880ba25e23b8beb23c44d988d6cf56bb969
mcortezv <mdjesuscv@gmail.com>
2025-10-18 22:07:15 -0700

    Categorias

--- frontend/src/pages/Hobbies.tsx
+++ frontend/src/pages/Hobbies.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import Section from "../componets/Section";
 import H1 from "../componets/H1";
 import Button from "../componets/Button";
+import Category from "../componets/Category";
 
 interface HobbiesProps {
   type: string;
@@ -22,6 +23,26 @@ const Hobbies = ({type}:HobbiesProps) => {
   return (
     <Section>
       <H1>Hobbies</H1>
+
+      <div className="mb-4 text-center">
+        <Category>Campus Centro</Category>
+        <Category>Club Lectura</Category>
+      </div>
+      <div className="mb-4 text-center">
+        <Category>Asesorias</Category>
+        <Category>Teatro</Category>
+        <Category>Alto Rendimiento</Category>
+      </div>
+      <div className="mb-4 text-center">
+        <Category>SA</Category>
+        <Category>Capus Nainari</Category>
+        <Category>Mobilidad</Category>
+      </div>
+      <div className="mb-4 text-center">
+        <Category>eSports</Category>
+        <Category>Personal Academico</Category>
+      </div>
+
       <Link to="/nextRegisterPage">
         <Button>&lt;</Button>
       </Link>
--- frontend/src/pages/Interests.tsx
+++ frontend/src/pages/Interests.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import Section from "../componets/Section";
 import H1 from "../componets/H1";
 import Button from "../componets/Button";
+import Category from "../componets/Category";
 
 interface InterestsProps {
   type: string;
@@ -21,6 +22,26 @@ const Interests = ({ type }: InterestsProps) => {
   return (
     <Section>
       <H1>Intereses</H1>
+
+      <div className="mb-4 text-center">
+        <Category>Musica</Category>
+        <Category>Deportes</Category>
+      </div>
+      <div className="mb-4 text-center">
+        <Category>Videojuegos</Category>
+        <Category>Bailar</Category>
+        <Category>Ajedrez</Category>
+      </div>
+      <div className="mb-4 text-center">
+        <Category>Cocinar</Category>
+        <Category>Peliculas</Category>
+        <Category>Tiro al Blanco</Category>
+      </div>
+      <div className="mb-4 text-center">
+        <Category>Programar</Category>
+        <Category>Cantar</Category>
+      </div>
+
       <Link to="/hobbiesPage">
         <Button>&lt;</Button>
       </Link>
