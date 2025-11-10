0cbea3785881b5504a67c58f2d4471b42ad372ca
SebastianEsc05 <sebastianescram01@gmail.com>
2025-11-10 15:24:55 -0700

    Prueba

--- backend/src/main/java/potronet/repositories/HobbieUsuarioRepository.java
+++ backend/src/main/java/potronet/repositories/HobbieUsuarioRepository.java
@@ -3,7 +3,11 @@ package potronet.repositories;
 import potronet.entities.HobbieUsuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import potronet.entities.Usuario;
+
+import java.util.Optional;
 
 @Repository
 public interface HobbieUsuarioRepository extends JpaRepository<HobbieUsuario,Long> {
+    void deleteByUsuario(Usuario usuario);
 }
