4310479d0ec9e389df8225519ce56ec3bd1ac420
mcortezv <mdjesuscv@gmail.com>
2025-11-10 13:16:37 -0700

    Sitemap

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,26 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { readFileSync } from "fs";
-import SitemapPlugin from "vite-plugin-sitemap";
 
 const routes = JSON.parse(readFileSync("./routes.json", "utf-8"));
 
 export default defineConfig({
   plugins: [
     react(),
-    SitemapPlugin({
-      hostname: "https://potro-net.vercel.app",
-      outDir: "dist",
-      dynamicRoutes: routes,
-      changefreq: "weekly",
-      priority: 0.8,
-      lastmod: new Date(),
-      robots: [
-        {
-          userAgent: "*",
-          allow: "/",
-        },
-      ],
-    }),
   ]
 });
